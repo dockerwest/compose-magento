a1adf23e537c0820163190ddbc75e701d8fb7a95
Ward Cappelle <wardcappelle@gmail.com>
2020-05-24 16:18:35 +0200

    Map file sync config to env and add helper methods
    Added methods will be required later on, allowing functions
    to be called only when Docker container is running, actual
    container name can be requested, general method to check existence
    of tools called upon ...

--- bin/environment.py
+++ bin/environment.py
@@ -8,6 +8,9 @@ import contextlib
 import codecs
 import re
 import shutil
+import sys
+import subprocess
+import time
 
 
 def split_env(env):
@@ -34,11 +37,10 @@ def env_vars_from_file(filename):
                 env[k] = v
     return env
 
-
 class Environment:
 
     required_keys = [
-        'BASEHOST', 'APPLICATION', 'WINDOW_MANAGER'
+        'BASEHOST', 'APPLICATION', 'WINDOW_MANAGER', 'APPLICATION_FILE_SYNC'
     ]
 
     def __init__(self, envfile):
@@ -50,9 +52,14 @@ class Environment:
             if requiredkey not in self.environment:
                 raise ValueError("Env does not contain %s" % requiredkey)
 
+    def is_tool_available(self, toolname):
+        from shutil import which
+        return which(toolname) is not None
+
     def get_compose_filename(self):
-        dingyexec = shutil.which('dinghy')
-        if None is not dingyexec:
+        if self.is_tool_available('mutagen'):
+            return 'docker-compose-mutagen.yml'
+        if self.is_tool_available('dinghy'):
             return 'docker-compose-dinghy.yml'
         return 'docker-compose.yml'
 
@@ -61,6 +68,72 @@ class Environment:
         basehost = self.get('BASEHOST')
         projectname = regex.sub('', basehost)
         return projectname
+
+    def get_container_id(self, container):
+        # TODO: might do with some additional checks, will break quite fast
+        container_id_cmd = [
+            os.path.dirname(sys.argv[0]) + '/run',
+            'ps', '-q', container
+        ]
+
+        container_id = subprocess.check_output(container_id_cmd, universal_newlines=True)
+        containerId = container_id.strip()
+
+        return containerId
+
+    def get_container_name(self, container):
+        container_id = self.get_container_id(container)
+
+        container_name_cmd = [
+            'docker',
+            'inspect', '-f', '{{.Name}}', container_id
+        ]
+
+        container_name = subprocess.check_output(container_name_cmd, universal_newlines=True)
+        container_name = container_name.strip().strip('/')
+
+        return container_name
+    
+    def is_container_running(self, container):
+        container_id = self.get_container_id(container)
+
+        if not container_id:
+            return False
+
+        container_status_cmd = [
+            'docker',
+            'inspect', '-f', '{{.State.Running}}', container_id
+        ]
+
+        container_status = subprocess.check_output(container_status_cmd, universal_newlines=True)
+        container_status = container_status.strip()
+
+        return container_status == 'true'
+    
+    def is_container_down(self, container):
+        return not self.is_container_running(container)
+
+    def await_container(self, container, state, timeout = 10, delay_before = 0, delay_after = 0):
+        req_state_map = {
+            'running': self.is_container_running,
+            'down': self.is_container_down
+        }
+
+        state_func = req_state_map.get(state, self.is_container_running)
+
+        if delay_before > 0:
+            time.sleep(delay_before)
+
+        daemon_runtime = 0
+        while not state_func(container):
+            daemon_runtime += 5
+            if timeout > 0 and daemon_runtime > timeout:
+                raise SystemExit(f'Container await timed out after {timeout}s, still not {state}.')
+
+            time.sleep(5)
+
+        if delay_after > 0:
+            time.sleep(delay_after)
 
     def get(self, key):
         if key not in self.environment:
